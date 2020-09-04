39d6f91fd92a51cb7dd92b58a05a17d321f6c9db
ricardopereira <ricardopereirasilveira@gmail.com>
2020-09-04 20:42:42 -0300

    Conversão do Vídeo e criação da página Result

---
+++ home/func.py
@@ -0,0 +1,8 @@
+from pytube import YouTube
+
+
+# TODO: Aqui é onde a conversão vai acontecer, será que é melhor criar em outro .py ?
+def videoConvert(url):
+    # url = url.download()
+    url = None
+    return url
--- home/views.py
+++ home/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render, redirect
 from pytube import YouTube
 
+from . import func
+
 
 # TODO: Implementar o metodo que faça a conversão do vídeo
 def index(request):
     # 'https://www.youtube.com/watch?v = KTji1hOICEI'
     link = request.GET.get('link-youtube', None)
+    video = None
     if link != None:
         yt = YouTube(link)
         video = yt.streams.order_by('resolution')[-1]
-        print(video)
-    return render(request, 'index.html')
+        video = func.videoConvert(video)
+        return redirect('result')
+    return render(request, 'index.html', {'video': video})
 
 
 # TODO: Aqui é a página que será redirecionada após receber a URL do passo anterior
 def result(request):
     return render(request, 'result.html')
-
-
-# TODO: Aqui é onde a conversão vai acontecer, será que é melhor criar em outro .py ?
-def videoConvert(url):
-    pass
