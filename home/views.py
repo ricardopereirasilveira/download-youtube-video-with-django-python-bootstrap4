cf087adcca94ce20c3e1cc65950ef540ff59caa7
ricardopereirasilveira <ricardopereirasilveira@gmail.com>
2020-09-09 21:40:44 -0300

    primeiro commit

--- home/views.py
+++ home/views.py
@@ -5,15 +5,25 @@ from . import func
 
 
 # TODO: Implementar o metodo que faça a conversão do vídeo
+# TODO: Implementar um LOG de vídeos baixados para poder informar
+#       na página posteriormente, com a função 'with open'
 def index(request):
+    """
+    Here we load the index website to user insert the link of video to download.
+    If the first visit, it dont access the IF.
+    After user insert the link and click in submit, it goes inside the IF to download
+        the video and the audio to convert then and give it to user download.
+    :param request: receive the requisition get from input in HTML ( with link )
+    :return: it return the request(without GET), return the page to load to user
+            and return the video var
+    """
     # 'https://www.youtube.com/watch?v = KTji1hOICEI'
     link = request.GET.get('link-youtube', None)
     video = None
     if link != None:
         yt = YouTube(link)
-        video = yt.streams.order_by('resolution')[-1]
-        video = func.videoConvert(video)
-        return redirect('result')
+        video = func.videoConvert(yt)
+        return render(request, 'result.html', {'video': video})
     return render(request, 'index.html', {'video': video})
 
 
