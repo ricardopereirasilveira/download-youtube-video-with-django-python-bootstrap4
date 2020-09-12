c0964bb13811b40f335cd2bab915dd424ccadcf0
ricardopereirasilveira <ricardopereirasilveira@gmail.com>
2020-09-12 00:21:05 -0300

    tentando ajustar um erro

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Download
-
-admin.site.register(Download)
-
-
-def __str__(self):
-    return self.link
--- home/func.py
+++ home/func.py
@@ -1,12 +1,13 @@
-from pytube import YouTube
+# from pytube import YouTube
 
 
 def videoDownload(url):
-    yt = YouTube(url)
-    # video = yt.streams.order_by('resolution')[-1].download()
-    audio = yt.streams.filter(only_audio=True)[-1].download()
-    title = yt.title
-    urlVideo = 'https://www.youtube.com/watch?v=' + yt.video_id
-    views = yt.views
-    thumbnail = yt.thumbnail_url
-    author = yt.author
+    # yt = YouTube(url)
+    # # video = yt.streams.order_by('resolution')[-1].download()
+    # audio = yt.streams.filter(only_audio=True)[-1].download()
+    # title = yt.title
+    # urlVideo = 'https://www.youtube.com/watch?v=' + yt.video_id
+    # views = yt.views
+    # thumbnail = yt.thumbnail_url
+    # author = yt.author
+    pass
