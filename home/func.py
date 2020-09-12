a8a44793db75f83fcfa13db481dcbe8f7b0d5256
ricardopereirasilveira <ricardopereirasilveira@gmail.com>
2020-09-11 23:38:42 -0300

    def function adjust to download video

--- home/func.py
+++ home/func.py
@@ -3,5 +3,10 @@ from pytube import YouTube
 
 def videoDownload(url):
     yt = YouTube(url)
-    titulo = yt.title
-    video = yt.streams.filter(re)
+    # video = yt.streams.order_by('resolution')[-1].download()
+    audio = yt.streams.filter(only_audio=True)[-1].download()
+    title = yt.title
+    urlVideo = 'https://www.youtube.com/watch?v=' + yt.video_id
+    views = yt.views
+    thumbnail = yt.thumbnail_url
+    author = yt.author
